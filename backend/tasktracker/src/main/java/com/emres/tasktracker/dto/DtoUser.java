ac288c1631f7ecd91b28849b78a094a5e0767fd5
soziki <mustafaemresozbir@gmail.com>
2026-07-11 14:59:47 +0300

    user dto

---
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/dto/DtoUser.java
@@ -0,0 +1,14 @@
+package com.emres.tasktracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DtoUser {
+  private String username;
+  
+  private String password;
+}
