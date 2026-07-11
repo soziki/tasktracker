591e8d62dda916e3d5a50ccc2770d2b1859909f8
soziki <mustafaemresozbir@gmail.com>
2026-07-11 14:58:00 +0300

    Helper classes for authenticate method

---
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/jwt/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.emres.tasktracker.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+  
+  private String token;
+
+  private String refreshToken;
+}
