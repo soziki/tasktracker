986652c3410b92440fa88e0f4444c3da8fd54071
soziki <mustafaemresozbir@gmail.com>
2026-07-11 03:01:29 +0300

    Request and response classes special for token refreshing

---
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/jwt/RefreshTokenRequest.java
@@ -0,0 +1,13 @@
+package com.emres.tasktracker.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+  
+  private String refreshToken;
+}
