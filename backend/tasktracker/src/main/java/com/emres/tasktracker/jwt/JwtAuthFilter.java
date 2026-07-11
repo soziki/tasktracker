c5df61561dd384debab75f56c5e3700129ec0590
soziki <mustafaemresozbir@gmail.com>
2026-07-11 14:55:45 +0300

    Authentication setting according to bearer token

---
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/jwt/JwtAuthFilter.java
@@ -0,0 +1,67 @@
+package com.emres.tasktracker.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter{
+  @Autowired
+  private JwtService jwtService;
+
+  @Autowired
+  private UserDetailsService userDetailsService; // to manage user stats etc. 
+  
+  @Override
+  protected void doFilterInternal(
+		HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+		throws ServletException, IOException {
+      
+      String header;
+      String token;
+      String username;
+
+      header = request.getHeader("Authorization"); // we did filter chaining and send back. We did not go down to controller.
+      if(null == header) {
+        filterChain.doFilter(request, response);
+        return;
+      }
+      token = header.substring(7);
+
+      try {
+        username = jwtService.getUsernameByToken(token);
+        if(null != username && null == SecurityContextHolder.getContext().getAuthentication()) {
+          UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+          if(null != userDetails && jwtService.isTokenValid(token)) {
+            
+            UsernamePasswordAuthenticationToken authantication = 
+            new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+
+            authantication.setDetails(userDetails);
+
+            SecurityContextHolder.getContext().setAuthentication(authantication);
+          }
+        }
+      } catch (ExpiredJwtException e) {
+        // TODO: handle exception
+        System.out.println("Token has been expired: " + e.getMessage());
+
+      } catch (Exception e) {
+        System.out.println("Something went wrong: " + e.getMessage());
+      }
+
+      filterChain.doFilter(request, response);
+    }
+}
