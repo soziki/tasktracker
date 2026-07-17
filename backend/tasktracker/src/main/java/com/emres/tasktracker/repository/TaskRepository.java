1756939d896568911030b4db4b6eee915919ad8e
soziki <mustafaemresozbir@gmail.com>
2026-07-17 10:07:53 +0300

    user's task lister method

--- backend/tasktracker/src/main/java/com/emres/tasktracker/repository/TaskRepository.java
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/repository/TaskRepository.java
@@ -3,6 +3,9 @@ package com.emres.tasktracker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.emres.tasktracker.model.Task;
+import java.util.List;
+
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+  public List<Task> findByTaskAssignedPersonIgnoreCase(String taskAssignedPerson);
 }
