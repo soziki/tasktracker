4d85b523722709d3276434cea1f8e1926ca71800
soziki <mustafaemresozbir@gmail.com>
2026-07-17 10:06:00 +0300

    oath2 resource server integrated with auth converter

--- backend/tasktracker/src/main/java/com/emres/tasktracker/config/SecurityConfig.java
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.emres.tasktracker.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import com.emres.tasktracker.jwt.JwtAuthConverter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+  @Autowired
+  private JwtAuthConverter jwtAuthConverter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,8 +25,11 @@ public class SecurityConfig {
     .authorizeHttpRequests((request) -> request.anyRequest().authenticated());
     
     //resource server
-    http.oauth2ResourceServer((server) -> server.jwt(Customizer.withDefaults()));
+    //http.oauth2ResourceServer((server) -> server.jwt(Customizer.withDefaults()));
 
+    //authConverter
+    http.oauth2ResourceServer((server) -> server.jwt((jwt) -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+    
     //session management 
     http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
