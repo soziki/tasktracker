659dca128ac04765321703fef17a409514a9e60b
soziki <mustafaemresozbir@gmail.com>
2026-07-17 10:03:23 +0300

    user only endpoint/method

--- backend/tasktracker/src/main/java/com/emres/tasktracker/controller/impl/TaskControllerImpl.java
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/controller/impl/TaskControllerImpl.java
@@ -3,8 +3,12 @@ package com.emres.tasktracker.controller.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.emres.tasktracker.controller.ITaskController;
 import com.emres.tasktracker.model.Task;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskControllerImpl implements ITaskController {
@@ -25,31 +30,55 @@ public class TaskControllerImpl implements ITaskController {
 
   @GetMapping("/")
   @Override
+  @PreAuthorize("hasAnyRole('client_admin', 'client_manager')")
   public List<Task> getAllTasks() {
     return taskService.getAllTasks();
   }
 
+  @GetMapping("/usr")
+  @Override
+  @PreAuthorize("hasRole('client_user')")
+  public List<Task> getTasksByKeycloakUsername() {
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    return taskService.getTasksByKeycloakUsername(username);
+  }
+
+  @GetMapping("/usr/{id}")
+  @Override
+  @PreAuthorize("hasRole('client_user')")
+  public Task getUserTaskById(@PathVariable(name = "id") Integer id) {
+    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    Task task = taskService.getUserTaskById(id, username);
+    if (task == null) {
+      throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    }
+    return task;
+  }
+
   @GetMapping("/{id}")
   @Override
+  @PreAuthorize("hasAnyRole('client_admin', 'client_manager')")
   public Task getTaskById(@PathVariable(name = "id") Integer id) {
       return taskService.getTaskById(id);
   }
 
-
   @PostMapping("/")
   @Override
+  @PreAuthorize("hasAnyRole('client_admin', 'client_manager')")
   public Task saveTask(@RequestBody Task task) {
       return taskService.saveTask(task);
   }
   
   @PutMapping("/{id}")
   @Override
+  @PreAuthorize("hasAnyRole('client_admin', 'client_manager')")
   public Task updateTask(@PathVariable(name = "id") Integer id, @RequestBody Task taskDetails) {      
       return taskService.updateTask(id, taskDetails);
   }
 
   @DeleteMapping("/{id}")
   @Override
+  @PreAuthorize("hasAnyRole('client_admin', 'client_manager')")
   public Task deleteTask(@PathVariable(name = "id") Integer Id) {
     return taskService.deleteTask(Id);
   }
--- backend/tasktracker/src/main/java/com/emres/tasktracker/service/ITaskService.java
+++ backend/tasktracker/src/main/java/com/emres/tasktracker/service/ITaskService.java
@@ -7,6 +7,10 @@ import com.emres.tasktracker.model.Task;
 public interface ITaskService {
   public List<Task> getAllTasks();
 
+  public List<Task> getTasksByKeycloakUsername(String username);
+
+  public Task getUserTaskById(Integer id, String username);
+
   public Task getTaskById(Integer id);
 
   public Task saveTask(Task task);
@@ -14,4 +18,5 @@ public interface ITaskService {
   public Task updateTask(Integer id, Task taskDetails);
 
   public Task deleteTask(Integer id);
+  
 }
